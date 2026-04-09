09f8191a524fb1ae740b7548a8993d09e4868969
Akhat <test@test.com>
2026-04-09 13:16:40 +0500

    fix test db endpoint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 import threading
-from datetime import datetime
 
 from app.models import *
-from app.core.db import Base, engine, SessionLocal
+from app.core.db import Base, engine
 
 from app.webhook_green import router as green_router
 
@@ -12,7 +11,6 @@ from app.api.auth import router as auth_router
 from app.api.account import router as account_router
 from app.api.payment import router as payment_router
 
-from app.models.account import Account
 from app.services.worker import run_worker
 
 
@@ -33,27 +31,3 @@ app.include_router(payment_router)
 @app.get("/")
 def root():
     return {"status": "ok"}
-
-
-@app.get("/test-db")
-def test_db():
-    db = SessionLocal()
-    try:
-        unique_email = f"test_{int(datetime.utcnow().timestamp())}@test.com"
-
-        acc = Account(
-            email=unique_email,
-            password_hash="test_hash_123"
-        )
-
-        db.add(acc)
-        db.commit()
-        db.refresh(acc)
-
-        return {
-            "status": "created",
-            "id": acc.id,
-            "email": acc.email
-        }
-    finally:
-        db.close()
